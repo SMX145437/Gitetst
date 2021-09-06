9db0fa949f1c5c0ccce17f2a79cac8f9f912fa45
SMX <1454375516@qq.com>
2021-09-06 15:42:25 +0800

    IDea commit 4

--- src/main/java/tt.java
+++ src/main/java/tt.java
@@ -1,9 +1,10 @@
 /**
  * @author sun
- * @creat 2021-09-06 15:24
+
  */
 public class tt {
     public static void main(String[] args) {
+        System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
     }
